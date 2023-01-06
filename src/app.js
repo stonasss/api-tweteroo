282b8f36771b10c7413cae65f071b80ac5a38ba0
stonasss <luis.b.santos96@gmail.com>
2023-01-06 17:56:35 -0300

    folder organization fix

---
+++ src/app.js
@@ -0,0 +1,54 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = 5000;
+
+const users = [];
+const tweets = [];
+let avatar = "";
+
+app.post("/sign-up", (req, res) => {
+  avatar = [];
+  const user = req.body;
+  avatar.push(user.avatar);
+  users.push(user);
+  console.log(user);
+  res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+  if (tweets.length >= 10) {
+    res.send(tweets.slice(0, 10));
+  }
+  res.send(tweets);
+});
+
+app.post("/tweets", (req, res) => {
+  console.log(tweets);
+
+  const tweet = req.body;
+  const message = tweet.tweet;
+  const currentUser = users.find((item) => item.username === tweet.username);
+
+  if (tweets.length == 0) {
+    tweet.avatar = avatar;
+    tweets.unshift(tweet);
+    res.send("OK");
+  } else if (message.length == 0) {
+    return res.status(401).send("Tweet vazio não é permitido");
+  } else if (currentUser) {
+    tweet.avatar = avatar;
+    tweets.unshift(tweet);
+    res.send("OK");
+  } else {
+    res.send("UNAUTHORIZED");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("servidor está aberto!");
+});
